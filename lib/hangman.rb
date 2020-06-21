d75787470b366299d6a24972159bc51367b1ae41
Axel Lopez <lopezaxel@protonmail.com>
2020-06-21 15:08:58 -0300

    Game works

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,18 +1,59 @@
+require "pry"
+
 class Game
-  attr_reader :dictionary, :player, :incorrect_guesses_left, :original_word
+  attr_reader :dictionary, :player, :dict_word, :guesses_limit
+  attr_accessor :loss
   def initialize(dictionary, player)
     @dictionary = dictionary
     @player = player
-    @incorrect_guesses_left = 6
-    @original_word = dictionary.word.split("")
+    @dict_word = dictionary.word.split("")
+    @guesses_limit = 12
+    @loss = false
   end
 
   def start_game
-    word = Array.new(original_word.length, "")
-    p word, original_word
-    # until
-    print player.prompt_enter_guess
-    player_letter = player.get_player_input
+    word_guess = copy_word_empty
+    guesses_left = guesses_limit
+
+    until loss
+      print player.prompt_enter_guess
+      player_letter = player.get_player_input
+
+      get_letter_matches(word_guess, player_letter)
+      guesses_left -= 1 unless dict_word.include?(player_letter)
+
+      give_feedback(word_guess, guesses_left)
+    end
+  end
+
+  def get_letter_matches(guess_word, player_letter)
+    dict_word.each_with_index do |letter, index|
+      if player_letter == letter
+        add_correct_letter(guess_word, player_letter, index)
+        remove_letter_dict(index)
+      end
+    end
+  end
+
+  def add_correct_letter(guess_word, letter, index)
+    guess_word[index] = letter
+  end
+
+  def remove_letter_dict(index)
+    dict_word[index] = ""
+  end
+
+  def copy_word_empty
+    Array.new(dict_word.length, "_")
+  end
+
+  def give_feedback(word, guesses_left)
+    puts "Correct guesses #{join_word(word)}"
+    puts "Incorrect guesses left #{guesses_left}\n"
+  end
+
+  def join_word(word)
+    word.join(" ")
   end
 end
 
